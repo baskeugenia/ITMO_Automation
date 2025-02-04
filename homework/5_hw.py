8f2bcedd4c8da635fc1ed5a6bbda67f31fddbc1a
Evgeiya Baskaeva <bask.eugenia@gmail.com>
2025-02-04 12:06:22 +0300

    homework 5 corrected

--- homework/5_hw.py
+++ homework/5_hw.py
@@ -2,17 +2,21 @@ from selenium import webdriver
 from selenium.webdriver.firefox.service import Service
 from selenium.webdriver.common.by import By
 
-service = Service()
-driver = webdriver.Firefox()
-driver.get("https://saucedemo.com/")
 
-try:
-    text_user_name = driver.find_element(By.CSS_SELECTOR, '#user-name')
-    text_password = driver.find_element(By.CSS_SELECTOR, '#password')
-    button = driver.find_element(By.CSS_SELECTOR, '#login-button')
+def get_sauce_page():
+    service = Service()
+    driver = webdriver.Firefox()
+    driver.get("https://saucedemo.com/")
 
-except:
-    print('Elements not found')
+    try:
+        text_user_name = driver.find_element(By.CSS_SELECTOR, '#user-name')
+        text_password = driver.find_element(By.CSS_SELECTOR, '#password')
+        button = driver.find_element(By.CSS_SELECTOR, '#login-button')
 
-else:
-    print("Elements found")
+    except:
+        print('Elements not found')
+
+    else:
+        print("Elements found")
+
+get_sauce_page()
